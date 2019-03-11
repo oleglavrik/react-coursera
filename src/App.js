9d64a400dbe5eeb2779465b1d074c1b9b0f15ae0
Oleh Lavryk <oleh.lavrik@gmail.com>
2019-03-11 06:08:32 +0200

    Configuring React, added Bootstrap 4.0.0 and Reactstrap 5.0.0

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Navbar} from "reactstrap";
+import {NavbarBrand} from "reactstrap";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">
+                            Brand name
+                        </NavbarBrand>
+                    </div>
+                </Navbar>
+            </div>
+        );
+    }
 }
 
 export default App;

613cb8472915eb295f6dd86a9300428aea7d2e99
Oleh Lavryk <oleh.lavrik@gmail.com>
2019-04-13 19:48:06 +0300

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -12,6 +12,7 @@ import RenderDish from './extra/RenderDishComponent';
 import {Link} from "react-router-dom";
 import {Control, LocalForm, Errors} from "react-redux-form";
 import {Loading} from './LoadingComponent';
+import { Stagger, Fade, } from 'react-animation-components';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -115,17 +116,21 @@ function RenderComments({comments, postComment, dishId}) {
             <React.Fragment>
                 <h4>Comments</h4>
                 <ul className="comments-list">
+                    <Stagger in>
                     {comments.map((comment) => {
                         return (
-                            <li key={comment.id} className="list-unstyled">
-                                <p>{comment.comment}</p>
-                                <p>
-                                    -- { comment.author },
-                                    {" " + new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                                </p>
-                            </li>
+                            <Fade in>
+                                <li key={comment.id} className="list-unstyled">
+                                    <p>{comment.comment}</p>
+                                    <p>
+                                        -- { comment.author },
+                                        {" " + new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                    </p>
+                                </li>
+                            </Fade>
                         );
                     })}
+                    </Stagger>
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment} />
             </React.Fragment>
--- src/components/extra/RenderDishComponent.js
+++ src/components/extra/RenderDishComponent.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import {Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
 import { baseUrl } from './../../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function RenderDish({dish}) {
     if(dish != null) {
         return (
-            <Card>
-                <CardImg src={baseUrl + dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform in transformProps={{exitTransform: 'scale(0.5) translateY(-50%)'}}>
+                <Card>
+                    <CardImg src={baseUrl + dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         );
     } else {
         return (
